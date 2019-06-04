72dd9d05b80fe89124cf5b64299cf18f7679d911
zhengbin <rjguanwen001@163.com>
2019-06-04 19:33:15 +0800

    confsvr init

---
+++ confsvr/src/main/java/cn/rjguanwen/cypress/confsvr/ConfsvrApplication.java
@@ -0,0 +1,13 @@
+package cn.rjguanwen.cypress.confsvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfsvrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfsvrApplication.class, args);
+    }
+
+}
